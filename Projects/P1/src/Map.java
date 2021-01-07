87988c73d257d4839455ee931148452a5eebc1a0
Daniel Rowe <daniel@drowe.dev>
2021-01-06 20:48:22 -0500

    implemented eatCookie

--- Projects/P1/src/Map.java
+++ Projects/P1/src/Map.java
@@ -71,6 +71,14 @@ public class Map{
 	public JComponent eatCookie(String name) {
 		//update locations, components, field, and cookies
 		//the id for a cookie at (10, 1) is tok_x10_y1
-		return null;
+		Location loc = this.locations.remove(name);
+		if (loc == null) {
+			return null;
+		}
+		JComponent comp = this.components.remove(name);
+		this.field.put(loc, emptySet);
+		this.cookies++;
+
+		return comp;
 	}
 }
