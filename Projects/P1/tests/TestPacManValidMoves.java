6eb7dd65b741d18e81dd9a38bc078adf4b0012da
ggarcz1 <54447564+ggarcz1@users.noreply.github.com>
2021-01-10 18:03:38 -0500

    Update TestPacManValidMoves.java
    still doesn't work, no return from the get_valid_moves function but better than nothing

--- Projects/P1/tests/TestPacManValidMoves.java
+++ Projects/P1/tests/TestPacManValidMoves.java
@@ -1,37 +1,27 @@
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
+import java.util.ArrayList;
 
 public class TestPacManValidMoves extends TestCase {
 
 	public void testPacManValidMoves() throws FileNotFoundException{
-		
-		boolean torf = true;
 		NoFrame frame = new NoFrame(); // Creates A New Map With Walls and Tokens w/o a Display
-		PacMan pacMan = frame.addPacMan(new Location(2, 2));
-		frame.initPlayers();
+		Ghost ghost = frame.addGhost(new Location(2, 2), "name", Color.red); //Creates a red ghost named "name" at location x,y
+		PacMan pacMan = frame.addPacMan(new Location(0, 0));
 		frame.startGame();
 
 		// Location loc = new Location(2, 2);
 		ArrayList<Location> arr = new ArrayList<Location>();
-		arr.add(new Location(3,2));
-		arr.add(new Location(2,3));
+		arr.add(new Location(2,1));
+		arr.add(new Location(1,2));
 
+		//returns null arraylist for some reason
 		ArrayList<Location> movespac = pacMan.get_valid_moves();
+		// movespac.add(new Location(2,1));
+		// movespac.add(new Location(1,2));
 
-		if (movespac.size() != arr.size()) {
-			torf = false;
-		} else {
-			for (int i = 0; i < arr.size(); i++) {
-				Location l = movespac.get(i);
-				if (!arr.contains(l)) {
-					torf = false;
-					break;
-				}
-				l = null;
-			}
-		}
-
-		assertTrue(torf);
+		// System.out.println(pacMan.get_valid_moves());
+		assertTrue(arr.equals(movespac));
 	}
 }
