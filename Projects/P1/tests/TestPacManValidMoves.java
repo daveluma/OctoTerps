93a00d91d111155e43ed685c5c0176920dd91743
ggarcz1 <54447564+ggarcz1@users.noreply.github.com>
2021-01-10 14:39:52 -0500

    Update TestPacManValidMoves.java

--- Projects/P1/tests/TestPacManValidMoves.java
+++ Projects/P1/tests/TestPacManValidMoves.java
@@ -5,6 +5,33 @@ import java.io.*;
 public class TestPacManValidMoves extends TestCase {
 
 	public void testPacManValidMoves() throws FileNotFoundException{
-		return null;
+		
+		boolean torf = true;
+		NoFrame frame = new NoFrame(); // Creates A New Map With Walls and Tokens w/o a Display
+		PacMan pacMan = frame.addPacMan(new Location(2, 2));
+		frame.initPlayers();
+		frame.startGame();
+
+		// Location loc = new Location(2, 2);
+		ArrayList<Location> arr = new ArrayList<Location>();
+		arr.add(new Location(3,2));
+		arr.add(new Location(2,3));
+
+		ArrayList<Location> movespac = pacMan.get_valid_moves();
+
+		if (movespac.size() != arr.size()) {
+			torf = false;
+		} else {
+			for (int i = 0; i < arr.size(); i++) {
+				Location l = movespac.get(i);
+				if (!arr.contains(l)) {
+					torf = false;
+					break;
+				}
+				l = null;
+			}
+		}
+
+		assertTrue(torf);
 	}
 }
