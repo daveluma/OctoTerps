b01874c0c28acc22b7903ea015653f308dfa983a
Will Spencer <willspencer805@gmail.com>
2021-01-11 16:57:19 -0500

    added extends to TestMapMove

--- Projects/P1/tests/TestMapMove.java
+++ Projects/P1/tests/TestMapMove.java
@@ -2,9 +2,9 @@ import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
 
-public class TestMapMove {
+public class TestMapMove extends TestCase {
 
-	public void testMapMove() {
+	public void testMapMove() throws FileNotFoundException {
 		NoFrame frame = new NoFrame();
 		Ghost ghost = frame.addGhost(new Location(2, 4), "Bobi", Color.MAGENTA);
 		PacMan pacman = frame.addPacMan(new Location(1, 1));
