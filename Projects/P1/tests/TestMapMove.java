b3fe6a6c7c86789a3b7c8d857f22c8cfaa05c4de
John Blanco <jomiaubl@gmail.com>
2021-01-11 15:15:50 -0500

    Implemented testMapMove() function for TestMapMove.java

--- Projects/P1/tests/TestMapMove.java
+++ Projects/P1/tests/TestMapMove.java
@@ -5,6 +5,12 @@ import java.io.*;
 public class TestMapMove {
 
 	public void testMapMove() {
-		return null;
+		NoFrame frame = new NoFrame();
+		Ghost ghost = frame.addGhost(new Location(2, 4), "Bobi", Color.MAGENTA);
+		PacMan pacman = frame.addPacMan(new Location(1, 1));
+		Map map = frame.getMap();
+
+		assertTrue(map.move("pacman", new Location (1,1), Map.Type.PACMAN));
+		assertTrue(map.move("Bobi", new Location(2, 4), Map.Type.GHOST));
 	}
 }
