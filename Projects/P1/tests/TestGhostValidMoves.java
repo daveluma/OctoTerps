2de4e2c684f0a26ce0e149817791bf79947ec743
ggarcz1 <54447564+ggarcz1@users.noreply.github.com>
2021-01-11 11:35:01 -0500

    Final Update valid moves

--- Projects/P1/tests/TestGhostValidMoves.java
+++ Projects/P1/tests/TestGhostValidMoves.java
@@ -7,21 +7,12 @@ public class TestGhostValidMoves extends TestCase {
 
 	public void testGhostValidMoves() throws FileNotFoundException {
 		NoFrame frame = new NoFrame(); // Creates A New Map With Walls and Tokens w/o a Display
-		Ghost ghost = frame.addGhost(new Location(2, 2), "name", Color.red); //Creates a red ghost named "name" at location x,y
-		PacMan pacMan = frame.addPacMan(new Location(1, 1));
-		frame.startGame();
-
-		// Location loc = new Location(2, 2);
+		Ghost ghost = frame.addGhost(new Location(1, 1), "name", Color.red); //Creates a red ghost named "name" at location x,y
+		PacMan pacMan = frame.addPacMan(new Location(3, 2));
 		ArrayList<Location> arr = new ArrayList<Location>();
 		arr.add(new Location(2,1));
 		arr.add(new Location(1,2));
-
-		//returns null arraylist for some reason
 		ArrayList<Location> ghostArr = ghost.get_valid_moves();
-		// ghostArr.add(new Location(2,1));
-		// ghostArr.add(new Location(1,2));
-
-		// System.out.println(pacMan.get_valid_moves());
 		assertEquals(arr, ghostArr);
 	}
 }
