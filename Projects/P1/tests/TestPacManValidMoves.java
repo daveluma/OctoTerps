23a7a9a1abdc29dcfaa4c8eb4385c11ba0c85ba2
ggarcz1 <54447564+ggarcz1@users.noreply.github.com>
2021-01-13 16:24:12 -0500

    update pacmanvalid moves test

--- Projects/P1/tests/TestPacManValidMoves.java
+++ Projects/P1/tests/TestPacManValidMoves.java
@@ -7,11 +7,36 @@ public class TestPacManValidMoves extends TestCase {
 
 	public void testPacManValidMoves() throws FileNotFoundException {
 		NoFrame frame = new NoFrame(); // Creates A New Map With Walls and Tokens w/o a Display
+		PacMan pacMan = frame.addPacMan(new Location(1, 1));
+		ArrayList<Location> arr = new ArrayList<Location>();
+		arr.add(new Location(2, 1));
+
+		ArrayList<Location> movespac = pacMan.get_valid_moves();
+		assertEquals(arr, movespac);
+	}
+
+	public void testPacManValidMoves2() throws FileNotFoundException {
+		NoFrame frame = new NoFrame(); // Creates A New Map With Walls and Tokens w/o a Display
 		Ghost ghost = frame.addGhost(new Location(3, 2), "name", Color.red); // Creates a red ghost named "name" at
 		PacMan pacMan = frame.addPacMan(new Location(1, 1));
 		ArrayList<Location> arr = new ArrayList<Location>();
 		arr.add(new Location(2, 1));
 		arr.add(new Location(1, 2));
+
+		ArrayList<Location> movespac = pacMan.get_valid_moves();
+		assertEquals(arr, movespac);
+	}
+	public void testPacManValidMoves3() throws FileNotFoundException {
+		NoFrame frame = new NoFrame(); // Creates A New Map With Walls and Tokens w/o a Display
+		// Ghost ghost = frame.addGhost(new Location(3, 2), "name", Color.red); // Creates a red ghost named "name" at
+		PacMan pacMan = frame.addPacMan(new Location(3, 2));
+		ArrayList<Location> arr = new ArrayList<Location>();
+		arr.add(new Location(3, 1));
+		arr.add(new Location(3, 3));
+		arr.add(new Location(4, 1));
+		arr.add(new Location(2, 1));
+		arr.add(new Location(4, 3));
+		
 		ArrayList<Location> movespac = pacMan.get_valid_moves();
 		assertEquals(arr, movespac);
 	}
