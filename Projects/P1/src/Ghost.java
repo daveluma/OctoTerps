9152e21468ba5f3884f160b630f23fa512f52263
Daniel Rowe <daniel@drowe.dev>
2021-01-15 15:08:35 -0500

    sabotaged GhostAttack

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -76,7 +76,7 @@ public class Ghost{
 	}
 
 	public boolean attack() {
-		if (is_pacman_in_range()) {
+		if (!is_pacman_in_range()) {
 			return this.myMap.attack(this.myName);
 		}
 		return false;
