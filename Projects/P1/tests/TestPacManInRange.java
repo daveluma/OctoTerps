b4660c38338b868ddf3290ca4c442810b12288ac
Will Spencer <willspencer805@gmail.com>
2021-01-17 17:58:45 -0500

    actually fixes tests

--- Projects/P1/tests/TestPacManInRange.java
+++ Projects/P1/tests/TestPacManInRange.java
@@ -7,8 +7,8 @@ public class TestPacManInRange extends TestCase {
 	public void testPacManInRange() throws FileNotFoundException{
 		NoFrame frame = new NoFrame();
 
-		PacMan pacMan = frame.addPacMan(new Location(4, 4));
-		Ghost ghost = frame.addGhost(new Location(1, 2), "Clyde", Color.BLACK);
+		PacMan pacMan = frame.addPacMan(new Location(1, 1));
+		Ghost ghost = frame.addGhost(new Location(3, 3), "Clyde", Color.BLACK);
 
 		assertTrue(!ghost.is_pacman_in_range());
 
